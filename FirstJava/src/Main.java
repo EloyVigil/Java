2a22584d2d4400c6f82d32ebabcbdc6fbed5cb5a
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-01-02 17:33:12 -0800

    Add files via upload

---
+++ FirstJava/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "My name is Eloy Vigil";
+        String myAge = "I am 36 years old";
+        String myHome = "My hometown is Willits, CA";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHome);
+    }
+}
